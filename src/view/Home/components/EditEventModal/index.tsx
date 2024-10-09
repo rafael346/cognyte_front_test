0bde72cc84492dc0fa5109a8cfc69e97b9da9944
Rafael Sousa do Nascimento <rafael.sousa@startaideia.com.br>
2024-10-08 21:16:57 -0300

    feat: update form on edit modal

--- src/view/Home/components/EditEventModal/index.tsx
+++ src/view/Home/components/EditEventModal/index.tsx
@@ -4,8 +4,15 @@ import { Input } from '../../../../components/Input';
 import Spacing from '../../../../components/Spacing';
 import Button from '../../../../components/Button';
 import Select from '../../../../components/Select';
+import { Controller, useForm } from 'react-hook-form';
 
 export default function EditModal() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm();
+
   return (
     <Modal
       isOpen
@@ -13,41 +20,84 @@ export default function EditModal() {
       description="Update event data and status"
       height="600px"
     >
-      <Input name="eventTitle" label="Event Name" value="" />
-      <div style={{ display: 'flex' }}>
-        <Input name="startDate" label="Start Date" />
-        <Spacing />
-        <Input name="startHour" label="Start Hour" />
-      </div>
-      <div style={{ display: 'flex' }}>
-        <Input name="endDate" label="End Date" />
-        <Spacing />
-        <Input name="endHour" label="End Hour" />
-      </div>
-      <Input name="price" label="Price" />
-      <Select
-        width="100%"
-        label="Event Status"
-        data={[
-          {
-            title: 'Started',
-            value: 1
-          },
-          {
-            title: 'Completed',
-            value: 2
-          },
-          {
-            title: 'Paused',
-            value: 3
+      <form
+        onSubmit={handleSubmit((data) => {
+          const start = new Date(data.startDate);
+          const end = new Date(data.endDate);
+          if (end <= start) {
+            alert('DATE RULE');
+          } else {
+            console.log(data);
           }
-        ]}
-        value={'city'}
-        onClick={(data) => /**/ {}}
-      />
-      <Button text="Update" width="100%" />
-      <Spacing />
-      <Button text="Cancel Update" width="100%" appearance="ghost" />
+        })}
+      >
+        <Controller
+          name="eventTitle"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <Input label="Event Name" {...field} />}
+        />
+
+        <div style={{ display: 'flex' }}>
+          <Controller
+            name="startDate"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => <Input label="Start Date" {...field} type="date" />}
+          />
+          <Spacing />
+          <Controller
+            name="startHour"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => <Input label="Start Hour" {...field} type="time" />}
+          />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <Controller
+            name="endDate"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => <Input label="End Date" {...field} type="date" />}
+          />
+          <Spacing />
+          <Controller
+            name="endHour"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => <Input label="End Hour" {...field} type="time" />}
+          />
+        </div>
+        <Controller
+          name="price"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <Input label="Price" {...field} />}
+        />
+        <Select
+          width="100%"
+          label="Event Status"
+          data={[
+            {
+              title: 'Started',
+              value: 1
+            },
+            {
+              title: 'Completed',
+              value: 2
+            },
+            {
+              title: 'Paused',
+              value: 3
+            }
+          ]}
+          value={'city'}
+          onClick={(data) => /**/ {}}
+        />
+        <Button text="Update" width="100%" />
+        <Spacing />
+        <Button text="Cancel Update" width="100%" appearance="ghost" />
+      </form>
     </Modal>
   );
 }
